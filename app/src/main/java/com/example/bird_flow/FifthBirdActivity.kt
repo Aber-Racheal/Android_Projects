95f695b1a0787b8a78f8d5b4ddc5929e5f05a142
Aber-Racheal <rachealaberr@gmail.com>
2024-06-16 06:58:19 +0300

    Added the binding view to all sections

---
+++ app/src/main/java/com/example/bird_flow/FifthBirdActivity.kt
@@ -0,0 +1,31 @@
+package com.example.hello
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hello.databinding.ActivityFifthBirdBinding
+import com.example.hello.databinding.ActivityFourthBirdBinding
+import com.squareup.picasso.Picasso
+
+class activity_fifth_bird : AppCompatActivity() {
+    lateinit var binding: ActivityFifthBirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFifthBirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.btnPrev2.setOnClickListener {
+            finish()
+        }
+        Picasso
+            .get()
+            .load("https://images.unsplash.com/photo-1480044965905-02098d419e96?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+            .centerCrop()
+            .resize(1500,1700)
+            .into(binding.imageView9)
+
+    }
+}
